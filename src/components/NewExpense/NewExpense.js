876bb6b857cc498672414b78fc2a1b0d284c082b
m-chagall <77046386+m-chagall@users.noreply.github.com>
2022-08-02 17:37:37 +0200

    Create NewExpense.js

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,19 @@
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      // this could generate same ids - we're just doing it for the sake of this demo.
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
