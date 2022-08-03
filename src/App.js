edd378c6499308572715cb23e767055bde368894
m-chagall <77046386+m-chagall@users.noreply.github.com>
2022-08-03 11:54:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
 // import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Rent",
+    amount: 1000,
+    date: new Date(2022, 7, 1),
+  },
+  {
+    id: "e2",
+    title: "Bills",
+    amount: 300,
+    date: new Date(2022, 7, 1),
+  },
+  {
+    id: "e3",
+    title: "Food",
+    amount: 800,
+    date: new Date(2022, 7, 1),
+  },
+  {
+    id: "e4",
+    title: "Transportation",
+    amount: 200,
+    date: new Date(2022, 7, 1),
+  },
+  {
+    id: "e5",
+    title: "Setup for WFH",
+    amount: 400,
+    date: new Date(2022, 7, 1),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Rent",
-      amount: 1000,
-      date: new Date(2022, 7, 1),
-    },
-    {
-      id: "e2",
-      title: "Bills",
-      amount: 300,
-      date: new Date(2022, 7, 1),
-    },
-    {
-      id: "e3",
-      title: "Food",
-      amount: 800,
-      date: new Date(2022, 7, 1),
-    },
-    {
-      id: "e4",
-      title: "Transportation",
-      amount: 200,
-      date: new Date(2022, 7, 1),
-    },
-    {
-      id: "e5",
-      title: "Setup for WFH",
-      amount: 400,
-      date: new Date(2022, 7, 1),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("in App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
   return (
     <div className="App">
