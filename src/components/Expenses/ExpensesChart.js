1abd6efa1b56344fb63dd517a4eef675d790bf5e
m-chagall <77046386+m-chagall@users.noreply.github.com>
2022-08-03 16:36:31 +0200

    Create ExpensesChart.js

---
+++ src/components/Expenses/ExpensesChart.js
@@ -0,0 +1,25 @@
+import Chart from "../Chart/Chart";
+
+const ExpensesChart = (props) => {
+  const chartDataPoints = [
+    { label: "JAN", value: 0 },
+    { label: "FEB", value: 0 },
+    { label: "MAR", value: 0 },
+    { label: "APR", value: 0 },
+    { label: "MAY", value: 0 },
+    { label: "JUN", value: 0 },
+    { label: "JUL", value: 0 },
+    { label: "AUG", value: 0 },
+    { label: "SEP", value: 0 },
+    { label: "OCT", value: 0 },
+    { label: "NOV", value: 0 },
+    { label: "DEC", value: 0 },
+  ];
+  for (const expense of props.expenses) {
+    const expenseMonth = expense.date.getMonth(); // starting at 0: January => [0]
+    chartDataPoints[expenseMonth].value += expense.amount;
+  }
+  return <Chart dataPoints={chartDataPoints} />;
+};
+
+export default ExpensesChart;
