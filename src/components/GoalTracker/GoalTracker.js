b7551fd038f9c1fb357ae2efd6ba6fcc24a10513
m-chagall <77046386+m-chagall@users.noreply.github.com>
2022-08-06 14:09:48 +0200

    Create GoalTracker.js

---
+++ src/components/GoalTracker/GoalTracker.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+import DUMMY_GOALS from "./Data/DummyGoals";
+import Goals from "./Goals/Goals";
+import NewGoal from "./NewGoal/NewGoal";
+
+const GoalTracker = () => {
+  /* ----------  Goal Tracker ---------- */
+  const [goals, setGoals] = useState(DUMMY_GOALS);
+
+  const addGoalHandler = (enteredText) => {
+    setGoals((prevGoals) => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = (goalId) => {
+    setGoals((prevGoals) => {
+      const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+  return (
+    <>
+      <h1>Goal Tracker</h1>
+      <NewGoal onAddGoal={addGoalHandler} />
+      <Goals items={goals} onDeleteItem={deleteItemHandler} />
+    </>
+  );
+};
+export default GoalTracker;
