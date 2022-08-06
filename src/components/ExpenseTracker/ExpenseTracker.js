835e0f9e77f26a7ce092b6c067e68d7054907a22
m-chagall <77046386+m-chagall@users.noreply.github.com>
2022-08-06 14:09:45 +0200

    Create ExpenseTracker.js

---
+++ src/components/ExpenseTracker/ExpenseTracker.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+import DUMMY_EXPENSES from "./Data/DummyExpenses";
+import Expenses from "./Expenses/Expenses";
+import NewExpense from "./NewExpense/NewExpense";
+
+const ExpenseTracker = () => {
+  /* ---------- Expense Tracker ---------- */
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  return (
+    <>
+      <h1>Expense Tracker</h1>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </>
+  );
+};
+export default ExpenseTracker;
