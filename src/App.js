81309d530d0a7a95dd3cecd41762b33cddc1c8d7
m-chagall <77046386+m-chagall@users.noreply.github.com>
2022-08-08 13:47:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import ExpenseTracker from "./components/ExpenseTracker/ExpenseTracker";
 import GoalTracker from "./components/GoalTracker/GoalTracker";
+import UserGenerator from "./components/UserGenerator/UserGenerator";
 
 const App = () => {
   return (
     <div className="App">
+      <UserGenerator />
+      <hr />
       <GoalTracker />
       <hr />
       <ExpenseTracker />
