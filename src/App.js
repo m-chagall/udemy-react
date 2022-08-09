331c7ba9c4741104d60ea49e3fadfdb491072587
m-chagall <77046386+m-chagall@users.noreply.github.com>
2022-08-09 15:53:51 +0200

    Update App.js
    Login w/ useContext

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import "./App.css";
+import { useContext } from "react";
+
 import ExpenseTracker from "./components/ExpenseTracker/ExpenseTracker";
 import GoalTracker from "./components/GoalTracker/GoalTracker";
 import UserGenerator from "./components/UserGenerator/UserGenerator";
 
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+
+import AuthContext from "./components/store/auth-context";
+
+import "./App.css";
+
 const App = () => {
+  const context = useContext(AuthContext);
   return (
-    <div className="App">
+    <>
+      <MainHeader />
+      <main>
+        {!context.isLoggedIn && <Login />}
+        {context.isLoggedIn && <Home />}
+      </main>
+
       <UserGenerator />
       <hr />
       <GoalTracker />
       <hr />
       <ExpenseTracker />
-    </div>
+    </>
   );
 };
 export default App;
