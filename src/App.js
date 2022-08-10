fe49853021145506bc206f9bf62d3e7b05c32235
m-chagall <77046386+m-chagall@users.noreply.github.com>
2022-08-10 10:19:58 +0200

    Update App.js
    reset for a new project

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
-import { useContext } from "react";
-
-import ExpenseTracker from "./components/ExpenseTracker/ExpenseTracker";
-import GoalTracker from "./components/GoalTracker/GoalTracker";
-import UserGenerator from "./components/UserGenerator/UserGenerator";
-
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
-
-import AuthContext from "./components/store/auth-context";
-
 import "./App.css";
 
 const App = () => {
-  const context = useContext(AuthContext);
-  return (
-    <>
-      <MainHeader />
-      <main>
-        {!context.isLoggedIn && <Login />}
-        {context.isLoggedIn && <Home />}
-      </main>
-
-      <UserGenerator />
-      <hr />
-      <GoalTracker />
-      <hr />
-      <ExpenseTracker />
-    </>
-  );
+  return <div className="App"></div>;
 };
 export default App;
